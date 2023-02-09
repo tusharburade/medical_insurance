77e437148b3afa94c0d085c5abfca89e9675dba3
unknown <trburade@gmail.com>
2023-02-09 21:33:06 +0530

    project folder

---
+++ project_app/utils.py
@@ -0,0 +1,33 @@
+import pickle
+import json
+import config
+import numpy as np
+
+class MedicalInsurance():
+    def __init__(self,age,sex,bmi,children,smoker,region):
+        self.age = age
+        self.sex = sex
+        self.bmi = bmi
+        self.children = children
+        self.smoker = smoker
+        self.region = 'region_'+region
+    
+    def load_model(self):
+        with open(config.MODEL_FILE_PATH,'rb') as f:
+            self.model = pickle.load(f)
+
+        with open(config.JSON_FILE_PATH,'r') as f:
+            self.json_data = json.load(f)
+    
+    def get_predicted_charges(self):
+        self.load_model()
+        region_index = self.json_data['columns'].index(self.region)
+        test_array = np.zeros(len(self.json_data['columns']))
+        test_array[0] = self.age
+        test_array[1] = self.json_data['sex'][self.sex]
+        test_array[2] = self.bmi
+        test_array[3] = self.children
+        test_array[4] = self.json_data['smoker'][self.smoker]
+        test_array[region_index] = 1
+        predicted_charges = self.model.predict([test_array])
+        return predicted_charges
